b7624b1b5fa0c2949dacded8ed274a62bf9b1781
Ajay <ajaydeep365@gmail.com>
2023-10-03 21:19:17 +0530

    second commit

--- src/main/java/com/springbootservices/dto/UserDTO.java
+++ src/main/java/com/springbootservices/dto/UserDTO.java
@@ -6,12 +6,12 @@ import jakarta.validation.constraints.NotEmpty;
 public class UserDTO {
 
     private Long id;
-    @NotEmpty
+    @NotEmpty(message = "User first name should not be null or empty")
     private String firstName;
-    @NotEmpty
+    @NotEmpty(message = "User last name should not be null or empty")
     private String lastName;
-    @NotEmpty
-    @Email
+    @NotEmpty(message = "User email should not be null or empty")
+    @Email(message = "User email should be valid")
     private String email;
 
     public UserDTO() {
--- src/main/java/com/springbootservices/exception/GlobalExceptionHandler.java
+++ src/main/java/com/springbootservices/exception/GlobalExceptionHandler.java
@@ -2,12 +2,17 @@ package com.springbootservices.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
@@ -42,8 +47,17 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
         return new ResponseEntity<>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(org.springframework.web.bind.MethodArgumentNotValidException ex, org.springframework.http.HttpHeaders headers, org.springframework.http.HttpStatusCode status,
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(org.springframework.web.bind.MethodArgumentNotValidException ex,
+                                                                  org.springframework.http.HttpHeaders headers,
+                                                                  org.springframework.http.HttpStatusCode status,
                                                                   org.springframework.web.context.request.WebRequest request){
-
+        Map<String,String> errors = new HashMap<>();
+        List<ObjectError> allErrors = ex.getBindingResult().getAllErrors();
+        allErrors.forEach((error)->{
+            String fieldName = ((FieldError)error).getField();
+            String message = error.getDefaultMessage();
+            errors.put(fieldName,message);
+        });
+        return new ResponseEntity<>(errors,HttpStatus.BAD_REQUEST);
     }
 }
